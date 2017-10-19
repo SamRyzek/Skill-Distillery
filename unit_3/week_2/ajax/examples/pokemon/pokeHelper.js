fed86744958b55bf0437303faa19396d380e6bbb
kkane106 <kkane106@gmail.com>
2017-10-19 13:49:03 -0600

    Remove

--- unit_3/week_2/ajax/examples/pokemon/pokeHelper.js
+++
@@ -1,6 +0,0 @@
-function capitalize(str) {
-  var result = "";
-  result += str.split("").shift().toUpperCase();
-  result += str.slice(1);
-  return result;
-}
--- unit_3/week_2/ajax/solutions/ch3/pokedex/app.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-  init();
-});
-
-var init = function(){
-  DataService.index(Display.buildTable);
-}
